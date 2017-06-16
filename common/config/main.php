5496deea2727c18b0245df49a70e101efc7a8d26
Shekhar <chandrashekhar.thalkar@infinitylabs.in>
2017-06-16 15:31:04 +0530

    Enable pretty URLs

--- common/config/main.php
+++ common/config/main.php
@@ -2,6 +2,15 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+		    'urlManager' => [
+		        'class' => 'yii\web\UrlManager',
+		        // Hide index.php
+		        'showScriptName' => false,
+		        // Use pretty URLs
+		        'enablePrettyUrl' => true,
+		        'rules' => [
+		        ],
+		    ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
