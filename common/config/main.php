99571e5303b2625466f1912e440e0de5f5e0aa8a
Shekhar <chandrashekhar.thalkar@infinitylabs.in>
2017-06-17 15:25:19 +0530

    Config yii2-user module in common config

--- common/config/main.php
+++ common/config/main.php
@@ -15,4 +15,12 @@ return [
             'class' => 'yii\caching\FileCache',
         ],
     ],
+    'modules' => [
+		    'user' => [
+		        'class' => 'dektrium\user\Module',
+		        // you will configure your module inside this file
+		        // or if need different configuration for frontend and backend you may
+		        // configure in needed configs
+		    ],
+		],
 ];
